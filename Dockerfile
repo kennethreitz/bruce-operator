c0bfe4464ef3e28258107eab64a2ed71278650fe
Kenneth Reitz <me@kennethreitz.org>
2018-09-30 15:06:59 -0400

    working i think

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ RUN echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/ap
 RUN apt-get update
 RUN apt-get install -y kubectl
 
+RUN mkdir -p /opt/buildpacks
+# VOLUME /opt/buildpacks
+
 COPY . /app
 RUN pip3 install -e .
 CMD bruce-operator watch
--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 docker build --tag kennethreitz/bruce-operator .
 docker push kennethreitz/bruce-operator
 kubectl delete -f .\deploy\operator.yml -n bruce
-kubectl create -f .\deploy\operator.yml -n bruce
+kubectl create -f .\deploy\operator.yml -n bruce --validate=false
